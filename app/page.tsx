cd2a1c5d09a758d518f64b666e6a87b0345be9e3
Franco Aparicio <ffrancoaparicio@gmail.com>
2024-05-30 13:40:48 -0300

    avances en grid about

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
-import Hero from "@/components/Hero";
 import { FloatingNav } from "@/components/ui/FloatingNav";
 import { FaHome } from "react-icons/fa";
+import Grid from "@/components/Grid";
+import Hero from "@/components/Hero";
 
 export default function Home() {
   return (
@@ -8,6 +9,7 @@ export default function Home() {
       <div className="max-w-7xl w-full">
         <FloatingNav navItems={[{name: 'Home', link: '/', icon: <FaHome />}]} />
         <Hero />
+        <Grid />
       </div>
     </main>
   );
